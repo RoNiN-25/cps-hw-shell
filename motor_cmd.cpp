bd48d0a7a3cfa995ac8dc52fbcd608d9e2176871
RoNiN-25 <aravindabhay@gmail.com>
2022-11-10 12:54:28 +0530

    Set motor connections properly. Tested with RSLK kit

--- motor_cmd.cpp
+++ motor_cmd.cpp
@@ -17,31 +17,31 @@ void set_motor_speed(char **cmd_strings, char* return_data) {
   char* motor = *(cmd_strings+1); // motor L or R
   char* percentage = *(cmd_strings+2); // speed percent
   int p = atoi(percentage);
-  int pwm = (int) (p*255);
-  if(strcmp(motor, "L")) {
+  int pwm = (int) (p/100.0*255);
+  if(!strcmp(motor, "L")) {
     // set left motor speed
     if (pwm == 0) {
       analogWrite(L_MOTOR_PWM_PIN, 0);
-      digitalWrite(L_MOTOR_DIR_PIN, HIGH);
+      digitalWrite(L_MOTOR_DIR_PIN, LOW);    // Sets left motor in forward direction. Depends on connections
       digitalWrite(L_MOTOR_SLEEP_PIN, LOW);  // give complement sleep to turn off motors
     } else if(pwm > 0 && pwm <= 255)  {
       analogWrite(L_MOTOR_PWM_PIN, pwm);
-      digitalWrite(L_MOTOR_DIR_PIN, HIGH);
-      digitalWrite(L_MOTOR_SLEEP_PIN, HIGH);
+      digitalWrite(L_MOTOR_DIR_PIN, LOW);     // Sets left motor in forward direction. Depends on connections
+      digitalWrite(L_MOTOR_SLEEP_PIN, HIGH);  // Keep motor ON
     } else {
       strcpy(return_data, "Invalid speed percentage\nPercentage must be between 0 and 100");
       return;
     }
-  } else if(strcmp(motor, "R")) {
+  } else if(!strcmp(motor, "R")) {
     // set right motor speed
     if (pwm == 0) {
       analogWrite(R_MOTOR_PWM_PIN, 0);
-      digitalWrite(R_MOTOR_DIR_PIN, HIGH);
+      digitalWrite(R_MOTOR_DIR_PIN, HIGH);   // Sets left motor in forward direction. Depends on connections
       digitalWrite(R_MOTOR_SLEEP_PIN, LOW);  // give complement sleep to turn off motors
     } else if(pwm > 0 && pwm <= 255)  {
       analogWrite(R_MOTOR_PWM_PIN, pwm);
-      digitalWrite(R_MOTOR_DIR_PIN, HIGH);
-      digitalWrite(R_MOTOR_SLEEP_PIN, HIGH);
+      digitalWrite(R_MOTOR_DIR_PIN, HIGH);   // Sets left motor in forward direction. Depends on connections
+      digitalWrite(R_MOTOR_SLEEP_PIN, HIGH); // Keep motor ON
     } else {
       strcpy(return_data, "Invalid speed percentage\nPercentage must be between 0 and 100");
       return;
--- motor_cmd.h
+++ motor_cmd.h
@@ -1,12 +1,12 @@
 #ifndef MOTOR_CMD_H_
 #define MOTOR_CMD_H_
 
-#define L_MOTOR_PWM_PIN 38      // PB_3
+#define L_MOTOR_SLEEP_PIN 38    // PB_3
 #define L_MOTOR_DIR_PIN 37      // PC_4
-#define L_MOTOR_SLEEP_PIN 36    // PC_5
-#define R_MOTOR_PWM_PIN 35      // PC_6
+#define L_MOTOR_PWM_PIN 36      // PC_5
+#define R_MOTOR_SLEEP_PIN 35    // PC_6
 #define R_MOTOR_DIR_PIN 34      // PC_7
-#define R_MOTOR_SLEEP_PIN 33    // PC_8
+#define R_MOTOR_PWM_PIN 33      // PD_6
 
 
 extern void set_motor_speed(char **cmd_strings, char* return_data);
